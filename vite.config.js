c12fa9fff813e1a99d0f8071e40db7f52e143062
shashwath63 <shashwathbn10@gmail.com>
2024-08-18 13:40:41 +0530

    jobs json data

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://localhost:8000',
+        target:'https://json-server-vercel-sigma-ten.vercel.app/',
         changeOrigin:true,
         rewrite:(path)=>path.replace(/^\/api/,'')
       }
